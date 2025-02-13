8c4c104aa6b3d224a18ddb371757ecb4cd84a13a
Ilia <kalinin499studing@gmail.com>
2025-02-13 12:10:17 +0300

    added requirements.txt and steps with remove footer, upload file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,9 @@ from selenium import webdriver
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_configuration():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     driver_options.add_argument('--headless')
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,8 +1,10 @@
+import os
 from selene import browser, have, be
 
 
 def test_fill_registration_form_with_mandatory_values():
-    browser.open('/')
+    browser.open('/automation-practice-form')
+    browser.element('footer').execute_script('element.remove()')
     browser.element('#firstName').should(be.blank)
 
     browser.element('#firstName').type('my_firstName')
@@ -26,7 +28,8 @@ def test_fill_registration_form_with_mandatory_values():
 
 
 def test_send_registration_form_with_all_values():
-    browser.open('/')
+    browser.open('/automation-practice-form')
+    browser.element('footer').execute_script('element.remove()')
     browser.element('#firstName').should(be.blank)
 
     browser.element('#firstName').type('my_firstName')
@@ -42,6 +45,7 @@ def test_send_registration_form_with_all_values():
 
     browser.element('#subjectsInput').type('hindi').press_enter().type('maths').press_enter()
     browser.element('#hobbies-checkbox-1 + .custom-control-label').click()
+    browser.element('#uploadPicture').type(os.path.abspath('resources/python.png'))
     browser.element('#currentAddress').type('my_curr_address')
 
     browser.element('#state').click().all("#state div").element_by(have.exact_text("Uttar Pradesh")).click()
@@ -52,13 +56,14 @@ def test_send_registration_form_with_all_values():
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.element('.table').all('td:nth-child(2)').should(have.texts(
         'my_firstName my_secondName', 'my_email@mail.com', 'Male', '8999123456', '21 June,2013',
-        'Hindi, Maths', '', '', 'my_curr_address', 'Uttar Pradesh Merrut'))
+        'Hindi, Maths', 'Sports', 'python.png', 'my_curr_address', 'Uttar Pradesh Merrut'))
     browser.element('#closeLargeModal').should(be.visible).click()
     browser.element('#firstName').should(be.blank)
 
 
 def test_send_empty_registration_form():
-    browser.open('/')
+    browser.open('/automation-practice-form')
+    browser.element('footer').execute_script('element.remove()')
     browser.element('#firstName').should(be.blank)
     browser.element('#submit').should(be.visible).click()
     browser.element('#example-modal-sizes-title-lg').should((have.no.text('Thanks for submitting the form')))
